92e3f8d189597f19f73adec31507f3a21387178d
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2025-04-06 03:45:19 -0400

    Update configuration files (babel, typescript, webpack)

--- webpack.config.js
+++ webpack.config.js
@@ -6,56 +6,62 @@ const host = 'localhost';
 const port = 8080;
 
 module.exports = {
-    mode: 'development',
-    context: path.resolve(__dirname, 'src'),
-    target: 'web',
-    entry: {
-        app: [
-            './index.tsx'
-        ]
-    },
-    output: {
-        filename: '[name]-[contenthash:6].bundle.js',
-        path: path.join(__dirname, './build/www'),
-        publicPath: `http://${host}:${port}/`
-    },
-    resolve: {
-        mainFields: ['browser', 'module', 'main'],
-        extensions: ['.js', '.json', '.jsx', '.ts', '.tsx']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.([jt])s(x?)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
-                use: 'file-loader?name=img/[name]-[contenthash:6].[ext]',
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name]-[contenthash:6].css',
-            chunkFilename: '[id].css'
-        })
-    ],
-    devServer: {
-        port,
-        host,
-        static: path.resolve(__dirname, 'src')
-    },
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  target: 'web',
+  entry: {
+    app: ['./index.tsx']
+  },
+  output: {
+    filename: '[name]-[contenthash:6].bundle.js',
+    path: path.join(__dirname, './build/www'),
+    publicPath: `http://${host}:${port}/`
+  },
+  resolve: {
+    mainFields: ['browser', 'module', 'main'],
+    extensions: ['.js', '.json', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.([jt])s(x?)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: 'file-loader?name=img/[name]-[contenthash:6].[ext]'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name]-[contenthash:6].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  devServer: {
+    port,
+    host,
+    static: path.resolve(__dirname, 'src'),
+    proxy: {
+      '/api': {
+        target: 'https://api.carbonintensity.org.uk',
+        secure: false,
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' }
+      }
+    }
+  }
 };
