cc4d7e0ea325782a129d89af14559336d4db747c
Stivens Carrasquel <stevensgoldzyzz@gmail.com>
2025-04-06 23:25:24 -0400

    api request headers modified

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,16 +1,12 @@
 import axios, { type AxiosInstance, type AxiosRequestConfig } from 'axios';
 
-const API_BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://api.carbonintensity.org.uk'
-    : (process.env.API_BASE_URL ?? '/api');
+const API_BASE_URL = process.env.API_BASE_URL ?? '/api';
 
 const axiosRequestConfig: AxiosRequestConfig = {
   baseURL: API_BASE_URL,
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
     Accept: 'application/json'
   }
 };
